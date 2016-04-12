c009a0f515dc269c4c9d06ba5605d753315f6969
clovery <clovery.org@gmail.com>
2016-04-12 11:14:15 +0800

    add upload component

---
+++ component/upload/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="upload">
+    <span role="button">
+      <input type="file" style="display: none">
+      <button type="button" class="btn btn-ghost"></button>
+      <i class="icon icon-upload"></i>
+      <span>点击上传</span>
+    </span>
+  </div>
+</template>
--- doc/doc.js
+++ doc/doc.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import menu from './menu.vue'
 import dropDown from '../example/drop-down.vue'
+import pagination from '../example/pagination.vue'
+import upload from '../example/upload.vue'
 
 require('../style/index.scss')
 require('./doc.scss')
@@ -13,16 +15,28 @@ new Vue({
         {
           label: 'drop-down',
           value: 'dropDown'
+        },
+        {
+          label: 'pagination',
+          value: 'pagination'
+        },
+        {
+          label: 'upload',
+          value: 'upload'
         }
       ],
-      currentComponent: 'dropDown'
+      currentComponent: 'upload'
     }
   },
   components: {
     menu,
 
     // example components
-    dropDown
+    dropDown,
+
+    pagination,
+
+    upload
   },
   methods: {
     setMenu(menu) {
