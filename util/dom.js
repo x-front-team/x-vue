dbb775ee5547e5bb3f3b809ce9a9dede0c8aa4df
Jokcy <jokcylou@hotmail.com>
2016-07-08 14:12:37 +0800

    merge

---
+++ util/dom.js
@@ -0,0 +1,11 @@
+export function getBodyScrollTop() {
+  let scrollTop
+  if (typeof window.pageYOffset !== 'undefined') { //pageYOffset指的是滚动条顶部到网页顶部的距离
+    scrollTop = window.pageYOffset
+  } else if (typeof document.compatMode !== 'undefined' && document.compatMode !== 'BackCompat') {
+    scrollTop = document.documentElement.scrollTop
+  } else if (typeof document.body !== 'undefined') {
+    scrollTop = document.body.scrollTop
+  }
+  return scrollTop
+}
---
+++ util/index.js
@@ -0,0 +1,5 @@
+import * as dom from './dom'
+
+export default {
+  dom
+}
