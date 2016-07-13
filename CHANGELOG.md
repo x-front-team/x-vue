fe7e5720bc6ede6db8c0667a7c2d832866803820
Jokcy <jokcylou@hotmail.com>
2016-07-13 16:41:37 +0800

    修改dropdown的问题

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# v-0.0.56 (2016-7-12)
+1. 本地打包发布在package-min的时候会出现模版字符串style里面的变量消失,结果是依赖包太老的问题
+
 # v-0.0.10 (2016-5-11)
 1. fix dropdown btn slot not toggle dropdown
 2. change btn slot to `span` not `a`
---
+++ example/drop-down/width.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="x-vue-demo-drop-down-base" class="clearfix">
+    <div v-show="show">
+      <x-drop-down btn-type="link" :label="'link'" id="x-vue-demo-drop-down-base-link">
+        <example type="link"></example>
+      </x-drop-down>
+    </div>
+    <x-button type="primary" @click="toggle">show drop down</x-button>
+    <!--<x-drop-down position="left" btn-type="primary" disabled :label="'primary'" id="x-vue-demo-drop-down-base-primary">-->
+      <!--<example type="primary"></example>-->
+    <!--</x-drop-down>-->
+    <!--<x-drop-down btn-type="success" :label="'success'" position="left" id="x-vue-demo-drop-down-base-success">-->
+      <!--<example type="success"></example>-->
+    <!--</x-drop-down>-->
+    <!--<x-drop-down btn-type="info" :label="'info'" id="x-vue-demo-drop-down-base-info">-->
+      <!--<example type="info"></example>-->
+    <!--</x-drop-down>-->
+    <!--<x-drop-down btn-type="danger" :label="'danger'" id="x-vue-demo-drop-down-base-danger">-->
+      <!--<example type="danger"></example>-->
+    <!--</x-drop-down>-->
+  </div>
+</template>
+<style lang="stylus" rel="stylesheet/stylus">
+  #x-vue-demo-drop-down-base{
+    .dropdown{
+      float: left;
+      margin-right: 10px;
+      margin-bottom: 10px;
+    }
+  }
+</style>
+<script type="text/babel">
+  export default {
+    data() {
+      return {
+        show: false
+      }
+    },
+    methods: {
+      toggle() {
+        debugger
+        this.show = !this.show
+      }
+    },
+    components: {
+      example: {
+        props: ['type'],
+        computed: {
+          text() {
+            return 'This is a drop down type of ' + this.type
+          }
+        },
+        template: '<x-card id="x-vue-demo-drop-down-base-card-{{type}}">' +
+        '<x-card-block>' +
+        '<x-card-title>{{type}}</x-card-title>' +
+        '<x-card-text>{{text}}</x-card-text>' +
+        '</x-card-block>' +
+        '</x-card>'
+      }
+    }
+  }
+</script>
